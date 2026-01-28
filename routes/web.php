3564addfc3e169d8ec64e3d0a243421ad2cbb4c2
Luís Silvério Calei Gabriel <132252803+luiscaleigabriel@users.noreply.github.com>
2026-01-28 17:29:12 +0100

    ....

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,7 @@
 <?php
 
 use App\Http\Controllers\SiteController;
+use App\Livewire\ShowTweets;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/', [SiteController::class, 'index'])->name('site.index');
-
-
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
-});
