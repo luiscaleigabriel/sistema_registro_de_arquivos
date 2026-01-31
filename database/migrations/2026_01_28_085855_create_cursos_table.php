7f8ac618c03ff713fe3e7d8c5b64e98e16d95b98
Luís Silvério Calei Gabriel <132252803+luiscaleigabriel@users.noreply.github.com>
2026-01-31 21:34:37 +0100

    ...

--- database/migrations/2026_01_28_085855_create_cursos_table.php
+++ database/migrations/2026_01_28_085855_create_cursos_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('cursos', function (Blueprint $table) {
             $table->id();
             $table->string('name')->unique();
+            $table->string('duracao');
             $table->timestamps();
         });
     }
