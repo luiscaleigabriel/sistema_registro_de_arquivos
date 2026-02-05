6829f99cc829a249481d4957a115a865bd4a49a4
Luís Silvério Calei Gabriel <132252803+luiscaleigabriel@users.noreply.github.com>
2026-02-05 05:38:50 +0100

    ...

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,12 +21,6 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
-        'photo',
-        'date_nac',
-        'morada',
-        'phone',
-        'bi',
-        'role',
     ];
 
     /**
@@ -47,25 +41,4 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
-
-    public function docs()
-    {
-        return $this->hasMany(Documento::class);
-    }
-
-    public function students()
-    {
-        return $this->hasMany(Estudante::class);
-    }
-
-    public function get_inscricao()
-    {
-        return $this->hasOne(Inscricao::class);
-    }
-
-    public function processo()
-    {
-        return $this->hasOne(Processo::class);
-    }
-
 }
--- routes/web.php
+++ routes/web.php
@@ -3,27 +3,16 @@
 use Illuminate\Support\Facades\Route;
 
 /*
-| Site
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
 */
 
-Route::livewire('/', 'pages::site.index')->name('site.index');
-Route::livewire('/sobre', 'pages::site.about')->name('site.about');
-Route::livewire('/cursos', 'pages::site.cursos')->name('site.cursos');
-Route::middleware(['auth', 'is_student'])->group( function() {
-    Route::livewire('/inscricao', 'pages::site.inscricao')->name('site.inscricao');
-});
-
-/*
-| Auth
-*/
-Route::middleware('guest')->group( function() {
-    Route::livewire('/login', 'pages::auth.login')->name('login');
-});
-
-/*
-| Student Dashboard
-*/
-
-Route::middleware(['auth', 'is_student'])->group( function() {
-    Route::livewire('/painel/aluno', 'pages::student.index')->name('student.index');
+Route::get('/', function () {
+    return view('welcome');
 });
